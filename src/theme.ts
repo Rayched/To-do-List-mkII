d68d3071455ae7225b0b8fc07ae18a108c2c9cf3
Rayched <ny2401@naver.com>
2024-08-13 12:56:01 +0900

    To do List: 초기 Project Setting

---
+++ src/theme.ts
@@ -0,0 +1,15 @@
+export const LightTheme = {
+    BgColor: "#dfe4ea",
+    ItemBgColor: "#f1f2f6",
+    textColor: "black",
+    accentColor: "#ffb142",
+    accentBgColor: "#d1ccc0",
+}
+
+export const DarkTheme = {
+    BgColor: "#1e272e", //배경색
+    ItemBgColor: "#485460", //컴포넌트 배경색
+    textColor: "white", //글자색
+    accentColor: "#8854d0", //하이라이트 시, 글자색
+    accentBgColor: "#a4b0be", //하이라이트 시, 배경색
+}
